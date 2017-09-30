2f93679acbae46639ea99bad80b9921cf808ea94
ngo275 <ngo38114@gmail.com>
2017-09-30 17:42:19 +0900

    Initial Commit

---
+++ CryptoArbitrage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoArbitrage
+//
+//  Created by Shuichi Nagao on 2017/09/30.
+//  Copyright © 2017 Shuichi Nagao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
